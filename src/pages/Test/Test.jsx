3379bfcfee7200f971672c38cb324a24f2d152a9
Dwi Sam <dwisamsiarto@gmail.com>
2023-06-14 09:41:56 +0700

    change algorithm

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import MBTIQuestion from "../../components/MBTIQuestions/MBTIQuestions";
 import Result from "../Result/Result";
 import questions from "../../data/Questions/Questions";
+import { calculateMBTIResult } from "../../utils/NaiveBayes";
 
 const Test = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -21,27 +22,8 @@ const Test = () => {
   };
 
   const calculateMBTI = () => {
-    const score = { I: 0, E: 0, S: 0, N: 0, T: 0, F: 0, J: 0, P: 0 };
-
-    // menghitung skor berdasarkan jawaban yang telah dipilih
-    for (const [questionId, answerId] of Object.entries(selectedAnswers)) {
-      const question = questions.find((q) => q.id === parseInt(questionId));
-      const answer = question.options.find((o) => o.id === answerId);
-      if (answer.type) {
-        for (const t of answer.type) {
-          score[t] += 1;
-        }
-      }
-    }
-
-    // menentukan hasil MBTI berdasarkan skor yang dihitung
-    let type = "";
-    type += score.I >= score.E ? "I" : "E";
-    type += score.S >= score.N ? "S" : "N";
-    type += score.T >= score.F ? "T" : "F";
-    type += score.J >= score.P ? "J" : "P";
-
-    setResult(type);
+    const result = calculateMBTIResult(selectedAnswers);
+    setResult(result);
   };
 
   return (
